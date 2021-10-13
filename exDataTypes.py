76be5ccc3257215929d061106e7b637387c35540
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-10-13 21:44:13 +0300

    third

--- exDataTypes.py
+++ exDataTypes.py
@@ -11,6 +11,7 @@ String Types
 """
 
 # Upper and Lower function
+"""
 print("Yusuf".upper(), "YUSUF".lower())
 
 print("dinimi".replace("i", "e"))
@@ -18,16 +19,29 @@ print("dinimi".replace("i", "e"))
 # Startswith and Endswith
 print("Yusuf".startswith("yu"))
 print("Yusuf".endswith("f"))
-
+"""
 # Split function split string given by to character
-print("Python NodeJs ReactJS ReactNative MongoDB".split(' '))
+#print("Python NodeJs ReactJS ReactNative MongoDB".split(' '))
 
 """
 Strip function trim string both side
 Lstrip trim from left
 Rstrip trim from right
 """
+"""
 text = "   Python   "
 print("Strip", text.strip())
 print("Lstrip", text.lstrip())
 print("Rstrip", text.rstrip())
+"""
+
+# join
+#print(".".join(["06", "11", "1998"]))
+
+# count function returns count of given character
+print("Yusuf".count("u"))
+print("Yusuf".count("u", 2))
+
+# find function return index of given character
+print("Yusuf".find("u"))
+print("Yusuf".rfind("u"))
