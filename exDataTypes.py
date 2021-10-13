cec3a4eaff04ac05e1da831587feb279a090dae3
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-10-13 21:33:35 +0300

    second

---
+++ exDataTypes.py
@@ -0,0 +1,33 @@
+# bin function returns num to binary
+#print("Binary versiyon", bin(18))
+
+# hex function returns num to hexadecimal
+#print("Hexadecimal versiyon", hex(18))
+
+# abs, pow, round, min, max
+
+"""
+String Types
+"""
+
+# Upper and Lower function
+print("Yusuf".upper(), "YUSUF".lower())
+
+print("dinimi".replace("i", "e"))
+
+# Startswith and Endswith
+print("Yusuf".startswith("yu"))
+print("Yusuf".endswith("f"))
+
+# Split function split string given by to character
+print("Python NodeJs ReactJS ReactNative MongoDB".split(' '))
+
+"""
+Strip function trim string both side
+Lstrip trim from left
+Rstrip trim from right
+"""
+text = "   Python   "
+print("Strip", text.strip())
+print("Lstrip", text.lstrip())
+print("Rstrip", text.rstrip())
