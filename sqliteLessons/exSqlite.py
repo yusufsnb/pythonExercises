0edd733dd202a6810e74ae56252066d62d7ec25a
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-10-14 21:24:12 +0300

    141021 last

--- sqliteLessons/exSqlite.py
+++ sqliteLessons/exSqlite.py
@@ -13,4 +13,32 @@ def createTable():
     con.commit()
 
 
+def insertToTable():
+    #cursor.execute("INSERT INTO users VALUES('Yusuf Sina', 'Bakan', 22)")
+    cursor.execute("INSERT INTO users VALUES(?,?,?)",
+                   ('Yusuf Sina', 'Bakan', 22))
+    con.commit()
+
+
+def selectDataFromTable():
+    cursor.execute("SELECT * FROM users")
+    liste = cursor.fetchall()
+    print(liste)
+
+
+def updateTable():
+    cursor.execute("UPDATE users SET age=23 WHERE age=22")
+    con.commit()
+
+
+def deleteFromTable():
+    cursor.execute("DELETE FROM users WHERE age=22")
+    con.commit()
+
+
+createTable()
+# insertToTable()
+# selectDataFromTable()
+# updateTable()
+
 con.close()
